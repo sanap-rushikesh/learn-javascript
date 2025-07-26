43598bbab01999a5fdafaa73cede3401fff3bb21
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-26 22:17:53 +0530

    learn-dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(2 + 2); // We are using nodejs, not browser
+
+console.log(3
+  +
+  3); // Code readability should be high
+
+console.log("Rushikesh");
+
+
+let name = "Rushikesh";
+let age = 23;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Rushikesh");
+console.log(typeof age);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
