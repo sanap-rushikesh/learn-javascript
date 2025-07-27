64c4a3c62171c837d1516a1023289313d08e227c
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-27 08:36:04 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // null is => 0
+// console.log(null == 0); // null is => null
+// console.log(null >= 0); // null is => 0
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+// console.log("2" = 2); // error
+console.log("2" == 2);
+console.log("2" === 2);
