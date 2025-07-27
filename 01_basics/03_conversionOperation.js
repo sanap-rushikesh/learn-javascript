9693d534c1b2a6ee0626293398f23329a27e2dbb
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-27 07:41:39 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = 33;
+let score2 = "43";
+let score3 = "53abc"
+let score4 = null;
+let score5 = undefined;
+let score6 = true;
+let score7 = "rushikesh";
+
+// console.log(typeof score);
+// console.log(typeof(score2));
+
+let valueInNumber = Number(score7);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1;
+let isLoggedIn2 = "";
+let isLoggedIn3 = "rushikesh";
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn3)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => falsse
+// "rushikesh" => true
+
+let someNumber1 = 33;
+
+let stringNumber = String(someNumber1);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
