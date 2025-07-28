734a7f83fad3d03b1d133416953f97222c5892fe
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-28 15:20:17 +0530

    scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,3 +15,42 @@ if(true){
 // console.log("Outer: ", a);
 // console.log(b);
 // console.log(c);
+
+
+// ************************************ Scope level and mini hoisting *************************************
+
+function one(){
+  const username = "rushikesh"
+
+  function two(){
+    const website = "youtube"
+    console.log(username);
+  }
+  // console.log(website);
+  // two();
+  
+}
+// one();
+
+if(true){
+  const username = "rushikesh";
+  if(username === "rushikesh"){
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  // console.log(website); 
+}
+// console.log(username);
+
+
+// **************************** hoisting **********************************
+console.log(addone(5));
+function addone(num){
+  return num + 1;
+}
+
+addTwo(5);
+const addTwo = function(num) {
+  return num + 2;
+}
+
