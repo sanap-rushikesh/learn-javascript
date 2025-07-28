f80f0d059370fa177ca677a9117d29c7db0bd372
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-28 13:36:53 +0530

    function with objects added

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -37,5 +37,39 @@ function loginUserMessage(username = "sam"){
   return `${username} just logged in`
 }
 
-console.log(loginUserMessage());
-console.log(loginUserMessage("rushikesh"));
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("rushikesh"));
+
+
+// ********************************** Function and Objects *****************************************
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 600, 800, 1000, 5000));
+
+const user = {
+  username: "rushikesh",
+  // price: 199
+  prices: 199
+}
+
+function handleObject(anyObject){
+  console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user);
+// handleObject({
+//   username: "sam",
+//   price: 199
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([300,600,900,1200,1500]));
