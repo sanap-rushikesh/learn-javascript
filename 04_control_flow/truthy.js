c4a9f4cdd9557eb8edfad7dc56c26dfdb96fa167
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-07-31 09:15:02 +0530

    truthy values falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,57 @@
+// const userEmail = "rushikesh.ai";
+const userEmail = "";
+// const userEmail = [];
+
+// if(userEmail){
+//   console.log("Got the user email");
+// }
+// else{
+//   console.log("Don't have user email");
+// }
+
+
+// Falsy Values: 
+// false, 0, -0, BigInt, " ", null, undefined, NaN;
+
+// Truthy Values:
+// "0", 'false', " ", [], {}, function(){};
+
+// if(userEmail.length === 0){
+//   console.log("Array is empty");
+// }
+
+// const emptyObj = {};
+// if(Object.keys(emptyObj).length === 0){
+//   console.log("Object is empty");
+// }
+
+// if(false == 0){
+//   console.log(`This condition is true`);
+// }
+// if(false == ''){
+//   console.log(`This condition is true`);
+// }
+// if('' == 0){
+//   console.log(`This condition is true`);
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+// val1 = null ?? 10 ?? 20;
+
+// console.log(val1);
+
+
+// Ternary Operator
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice <=80 ? console.log(`Less than 80`) : console.log(`More than 80`);
+
+
