cbac395bc04b55d1716c2b19f4479243fe3d000c
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-08-04 23:39:30 +0530

    second half-project BMI Calculator

---
+++ 07_projects/2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('form')
+const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+  const height = parseInt(document.querySelector('#height').value)
+  const weight = parseInt(document.querySelector('#weight').value)
+
+})
